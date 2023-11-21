c9e1ffb81b76238dc10650b2b6e8dfb8510b8747
saidabdul80 <saidabdulsalam5@gmail.com>
2023-11-21 10:04:30 +0100

    fix: updates

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,15 +1,23 @@
 describe('Navigation', () => {
-    it('should navigate to the about page', () => {
-      // Start from the index page
-      cy.visit('http://localhost:3000/')
-   
-      // Find a link with an href attribute containing "about" and click it
-      cy.get('a[href*="users"]').click()
-   
-      // The new url should include "/about"
-      cy.url().should('include', '/users')
-   
-      // The new page should contain an h1 with "About page"
-      //cy.get('h1').contains('About Page')
-    })
+  it('should navigate to the about page', () => {
+    // Start from the index page
+    cy.visit('http://localhost:3000/')
+    
+    // Find a link with an href attribute containing "/posts" and click it
+    cy.get('a[href*="/posts"]').click()
+
+    
+    // Find a link to create a new post and click it
+    cy.get('a[href*="/posts/manage/new"]').click()
+
+    // Go back to the posts page
+    cy.visit('http://localhost:3000/posts')
+
+    // Click on a specific post link
+    cy.get('a[href*="/posts"]').eq(1).click()
+
+    cy.visit('http://localhost:3000/dashboard')
+    cy.visit('http://localhost:3000/users')
+
   })
+})
